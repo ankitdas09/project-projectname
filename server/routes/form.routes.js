65e3932babe2f3361c689fcf9633f80db5404bae
Ankit <ankit@Ankits-MacBook-Air.local>
2022-07-18 13:10:26 +0530

    Added form routes with server side form validation

--- server/routes/form.routes.js
+++ server/routes/form.routes.js
@@ -4,6 +4,7 @@ const isAuthenticated = require('../middleware/isAuthenticated.middleware')
 const AppError = require('../utils/AppError')
 const catchAsync = require('../utils/catchAsync')
 
+
 const posts = []
 
 const schema = Joi.object({
@@ -12,6 +13,7 @@ const schema = Joi.object({
     weight: Joi.number().required(),
     age: Joi.number().required(),
     height: Joi.number().required(),
+    bloodSugar: Joi.number().required(),
 }).required()
 
 router.post('/', isAuthenticated, catchAsync(async (req, res, next) => {
